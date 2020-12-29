371e5e3b119eb81d2b6bae170da2dd69da2c5d1f
dznbk <n.douzono@gmail.com>
2020-12-30 03:26:57 +0900

    add gitignore and recipe change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const app = new Vue({
     el: '#app',
     data: {
-        "recipe": "何もない"
+        "recipe": "何もない",
+        "recipe_list": ["天丼","牛丼","親子丼"],
+        "counter": 0,
     },
     methods: {
         change: function (event, value) {
-            this.recipe = "change";
+            this.counter = (this.counter + 1) % this.recipe_list.length;
+            this.recipe = this.recipe_list[this.counter];
         }
     }
 })
